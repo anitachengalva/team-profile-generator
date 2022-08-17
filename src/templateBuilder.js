e0d614ebce8a1fbee677fb1651083cb9825860ab
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-16 23:56:23 -0700

    created different cards for manager, engineer, and intern

--- src/templateBuilder.js
+++ src/templateBuilder.js
@@ -1,9 +1,42 @@
-const fs = require('fs')
+const fs = require('fs');
+const Engineer = require('../lib/engineer');
+const Intern = require('../lib/intern');
+const Manager = require('../lib/manager');
 
 function render(employees){
     let cardStack = "";
 
-    for(employee of employees){
+    for(Manager of employees){
+        cardStack+=
+        `
+        <div class="card">
+            <h2 class="card-name">${employees.name}</h2>
+            <h3 class="card-role">${employees.role}</h3>
+            <ul class="card-details">Employee ID: ${employees.id}</ul>
+            <ul class="card-details">Office Number: ${employees.officeNumber}</ul>
+            <ul class="card-details">School: ${employees.school}</ul>
+            <ul class="card-details">Email: <a href="mailto:${employees.email}">${employees.email}</a></ul>
+            <ul class="card-details">GitHub: <a href="https://github.com/${employees.github}" target="_blank">${employees.github}</a></ul>
+        </div>
+        `
+    }
+
+    for(Engineer of employees){
+        cardStack+=
+        `
+        <div class="card">
+            <h2 class="card-name">${employees.name}</h2>
+            <h3 class="card-role">${employees.role}</h3>
+            <ul class="card-details">Employee ID: ${employees.id}</ul>
+            <ul class="card-details">Office Number: ${employees.officeNumber}</ul>
+            <ul class="card-details">School: ${employees.school}</ul>
+            <ul class="card-details">Email: <a href="mailto:${employees.email}">${employees.email}</a></ul>
+            <ul class="card-details">GitHub: <a href="https://github.com/${employees.github}" target="_blank">${employees.github}</a></ul>
+        </div>
+        `
+    }
+
+    for(Intern of employees){
         cardStack+=
         `
         <div class="card">
