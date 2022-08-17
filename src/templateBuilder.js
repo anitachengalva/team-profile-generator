297e0b771105984bc2e649fa378dcde94f8c89e6
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-16 23:54:34 -0700

    updated cardstack for default employee

--- src/templateBuilder.js
+++ src/templateBuilder.js
@@ -4,10 +4,18 @@ function render(employees){
     let cardStack = "";
 
     for(employee of employees){
-        cardstack+=`
+        cardStack+=
+        `
         <div class="card">
-        <h2>Name: ${employee.name}
-    </div>`
+            <h2 class="card-name">${employees.name}</h2>
+            <h3 class="card-role">${employees.role}</h3>
+            <ul class="card-details">Employee ID: ${employees.id}</ul>
+            <ul class="card-details">Office Number: ${employees.officeNumber}</ul>
+            <ul class="card-details">School: ${employees.school}</ul>
+            <ul class="card-details">Email: <a href="mailto:${employees.email}">${employees.email}</a></ul>
+            <ul class="card-details">GitHub: <a href="https://github.com/${employees.github}" target="_blank">${employees.github}</a></ul>
+        </div>
+        `
     }
 
     let finalHTML = 
