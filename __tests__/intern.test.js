3cfa0b3c80e4e5f9f176642a1e023d975b3c7429
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-24 23:44:58 -0700

    updated tests

--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -1,1 +1,12 @@
 // jest test
+
+const Intern = require("../lib/intern")
+
+
+
+describe("Testing to Check Intern Profile for a School",()=>{
+    test("Interns should have a School listed",()=>{
+        let testEmployee = new Intern("Student",2,"a@email.com","here's a school")
+        expect(testEmployee.school).toBe("here's a school")
+    })
+})
--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -1,1 +1,12 @@
 // jest test
+
+const Manager = require("../lib/manager")
+
+
+
+describe("Testing to Check Manager Profile for an Office",()=>{
+    test("Managers should have an Office number listed",()=>{
+        let testEmployee = new Manager("Manager",2,"a@email.com","12345")
+        expect(testEmployee.officeNumber).toBe("12345")
+    })
+})
